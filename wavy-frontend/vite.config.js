4693879c3d97710e52111fac2aac3f72c26b83ee
siddharth756 <siddharthsarvaiya56@gmail.com>
2025-04-14 18:27:01 +0530

    Initial modified.

--- wavy-frontend/vite.config.js
+++ wavy-frontend/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+   // 👇 Add this section to fix 404 on refresh
+   resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: '/',
 })
